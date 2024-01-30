28e8ed77e7dbc9a1ce403bc3c5659ce71216aa38
DELL <DELL@DESKTOP-J003PRJ>
2024-01-31 00:54:07 +0530

    Test case 1 passed successfully

--- Git_hub/src/testcases/Testcase1.java
+++ Git_hub/src/testcases/Testcase1.java
@@ -4,6 +4,6 @@ public class Testcase1 {
 
 	public static void main(String[] args) {
 	System.out.println("Mr.Nageshwar intellegentfellow");
-	System.out.println("Navya garu good person");
+	System.out.println("Navya garu good hearted person");
 	}
 }
