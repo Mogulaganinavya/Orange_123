1b3fe6e989f3874ce626d44bd23b85a7b9b2adfd
mohammed shajahan <mohammed shajahan@192.168.1.23>
2024-01-31 01:03:10 +0530

    testcase2 updated

--- Git_hub/src/testcases/Testcase2.java
+++ Git_hub/src/testcases/Testcase2.java
@@ -5,5 +5,6 @@ public class Testcase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Navya Garu Is good hearted person...");
+      System.out.println("nageshwar rao is most handsome boy");
 	}
 }
